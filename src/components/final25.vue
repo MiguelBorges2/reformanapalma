b437d1be36bd9c53eecadb147123d61909a2ec11
Miguel <miguel.costa@ufu.br>
2024-12-06 00:41:50 -0300

    Corrigindo caminho do arquivo de imagem

--- src/components/final25.vue
+++ src/components/final25.vue
@@ -2,8 +2,8 @@
       <div class="d-flex flex-column align-items-center spain justify-content-around">
          <p class="text-light lm zabata">Essas informações foram uteis?</p>
          <div class="d-flex justify-content-center align-items-center zaba">
-            <img class="tamamp" src="C:\Users\labor\Desktop\teste2\src\assets\satisfeito 1.svg">
-            <img class="tamamp" src="C:\Users\labor\Desktop\teste2\src\assets\insatisfeito 1.svg">
+            <img class="tamamp" src="@assets/satisfeito 1.svg">
+            <img class="tamamp" src="@assets/insatisfeito 1.svg">
          </div>
          <p class="text-light zabata">Antes de continuar, Você teve algum outro problema? </p>
          <p class="text-light zabata2  "> Clique no botão abaixo e preencha o formulário para ajudar a melhorar os conteúdos da plataforma</p>
@@ -18,7 +18,7 @@
 </script>
 <style>
 .spain{
-   
+
    background: linear-gradient(to top, #749695, #394A55); 
    border-radius: 60px 60px 0px 0px;
 }
