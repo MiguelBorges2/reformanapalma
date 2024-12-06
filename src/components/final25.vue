ab14e849dd79a7639d97a7a7e3a74facc6667f63
Miguel <miguel.costa@ufu.br>
2024-12-06 00:45:53 -0300

    Corrigindo arquivos ausentes

--- src/components/final25.vue
+++ src/components/final25.vue
@@ -2,8 +2,8 @@
       <div class="d-flex flex-column align-items-center spain justify-content-around">
          <p class="text-light lm zabata">Essas informações foram uteis?</p>
          <div class="d-flex justify-content-center align-items-center zaba">
-            <img class="tamamp" src="@assets/satisfeito 1.svg">
-            <img class="tamamp" src="@assets/insatisfeito 1.svg">
+            <img class="tamamp" src="@/assets/satisfeito 1.svg">
+            <img class="tamamp" src="@/assets/insatisfeito 1.svg">
          </div>
          <p class="text-light zabata">Antes de continuar, Você teve algum outro problema? </p>
          <p class="text-light zabata2  "> Clique no botão abaixo e preencha o formulário para ajudar a melhorar os conteúdos da plataforma</p>
